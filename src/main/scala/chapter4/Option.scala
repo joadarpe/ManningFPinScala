915dd91904ac6dd74763bc0ff49a48aaf94e2419
jonathana <jaap5227@gmail.com>
2018-08-16 15:28:29 -0500

    Adding exercises 4.2 -> 4.5

--- src/main/scala/chapter4/Option.scala
+++ src/main/scala/chapter4/Option.scala
@@ -2,6 +2,19 @@ package chapter4
 
 sealed trait Option[+A] {
 
+  /**
+    * Exercise 4.1
+    * Implement all of the preceding functions on Option. As you implement each function,
+    * try to think about what it means and in what situations you’d use it.
+    * We’ll explore when to use each of these functions next.
+    * Here are a few hints for solving this exercise:
+    *
+    * - It’s fine to use pattern matching, though you should be able to implement all the functions besides map
+    * and getOrElse without resorting to pattern matching.
+    * - For map and flatMap, the type signature should be enough to determine the implementation.
+    * - getOrElse returns the result inside the Some case of the Option, or if the Option is None, returns the given default value.
+    * - orElse returns the first Option if it’s defined; otherwise, it returns the second Option.
+    */
   def map[B](f: A => B): Option[B] = this match {
     case None => None
     case Some(a) => Some(f(a))
@@ -34,5 +47,58 @@ object Option {
   def mean(xs: Seq[Double]): Option[Double] = {
     if (xs.isEmpty) None
     else Some(xs.sum / xs.length)
+  }
+
+  /**
+    * Exercise 4.2
+    * Implement the variance function in terms of flatMap.
+    * If the mean of a sequence is m, the variance is the mean of math.pow(x - m, 2) for each element x in the sequence.
+    */
+  def variance(xs: Seq[Double]): Option[Double] = {
+    mean(xs) flatMap (m => mean(xs.map(x => math.pow(x - m, 2))))
+  }
+
+  /**
+    * Exercise 4.3
+    * Write a generic function map2 that combines two Option values using a binary function.
+    * If either Option value is None, then the return value is too
+    */
+  def mapPM[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = (a, b) match {
+    case (_, None) => None
+    case (None, _) => None
+    case (Some(a), Some(b)) => Some(f(a, b))
+  }
+
+  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = {
+    a flatMap (aa => b map (bb => f(aa, bb)))
+  }
+
+  /**
+    * Exercise 4.4
+    * Write a function sequence that combines a list of Options into one Option containing a list of all the Some values
+    * in the original list. If the original list contains None even once, the result of the function should be None;
+    * otherwise the result should be Some with a list of all the values.
+    */
+  def sequence[A](a: List[Option[A]]): Option[List[A]] = a match {
+    case Nil => Some(Nil)
+    case h :: t => h flatMap (hh => sequence(t) map (tt => hh :: tt))
+  }
+
+  /**
+    * Exercise 4.5
+    * Implement this function. It’s straightforward to do using map and sequence, but try for a more efficient implementation
+    * that only looks at the list once. In fact, implement sequence in terms of traverse.
+    */
+  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = {
+    sequence(a map f)
+  }
+
+  def traversePM[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = a match {
+    case Nil => Some(Nil)
+    case h :: t => map2(f(h), traversePM(t)(f))(_ :: _)
+  }
+
+  def sequenceT[A](a: List[Option[A]]): Option[List[A]] = {
+    traversePM(a)(x => x)
   }
 }
--- src/test/scala/chapter4/OptionTest.scala
+++ src/test/scala/chapter4/OptionTest.scala
@@ -4,19 +4,6 @@ import org.scalatest.{FreeSpec, Matchers}
 
 class OptionTest extends FreeSpec with Matchers {
 
-  /**
-    * Exercise 4.1
-    * Implement all of the preceding functions on Option. As you implement each function,
-    * try to think about what it means and in what situations you’d use it.
-    * We’ll explore when to use each of these functions next.
-    * Here are a few hints for solving this exercise:
-    *
-    * - It’s fine to use pattern matching, though you should be able to implement all the functions besides map
-    * and getOrElse without resorting to pattern matching.
-    * - For map and flatMap, the type signature should be enough to determine the implementation.
-    * - getOrElse returns the result inside the Some case of the Option, or if the Option is None, returns the given default value.
-    * - orElse returns the first Option if it’s defined; otherwise, it returns the second Option.
-    */
   "Exercise 4.1 map" in {
     None.map(a => a) shouldBe None
     Some(5).map(a => a.toString) shouldBe Some("5")
@@ -43,6 +30,31 @@ class OptionTest extends FreeSpec with Matchers {
     None.filter(_ => false) shouldBe None
     Some(5).filter(_ == 5) shouldBe Some(5)
     Some(5).filter(_ == 6) shouldBe None
+  }
+
+  "Exercise 4.2 variance" in {
+    Option.mean(Seq(3.0, 3.0, 3.0)) shouldBe Some(3.0)
+    Option.variance(Seq(3.0, 3.0, 3.0)) shouldBe Some(0.0)
+  }
+
+  "Exercise 4.3 map2" in {
+    val sum = (a: Int, b: Int) => a + b
+    Option.map2(None, None)(sum) shouldBe None
+    Option.map2(None, Some(1))(sum) shouldBe None
+    Option.map2(Some(1), None)(sum) shouldBe None
+    Option.map2(Some(1), Some(2))(sum) shouldBe Some(3)
+  }
+
+  "Exercise 4.4 sequence" in {
+    Option.sequence(List(Some(1), Some(2), None)) shouldBe None
+    Option.sequence(List(Some(1), Some(2), Some(3))) shouldBe Some(List(1, 2, 3))
+  }
+
+  "Exercise 4.5 traverse" in {
+    Option.traverse(List(1, 2, 3))(a => Some(a.toString)) shouldBe Some(List("1", "2", "3"))
+    Option.traversePM(List(1, 2, 3))(a => Some(a.toString)) shouldBe Some(List("1", "2", "3"))
+    Option.sequenceT(List(Some(1), Some(2), None)) shouldBe None
+    Option.sequenceT(List(Some(1), Some(2), Some(3))) shouldBe Some(List(1, 2, 3))
   }
 
 }
